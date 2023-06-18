d523864f6afbc5c0a15d3017bc0191fbc35c62f8
Abdelrhaman Elhetamy <elhetamy98@gmail.com>
2023-06-19 02:22:58 +0300

    Timers driver under development

---
+++ MCAL/6-Timers/TIMERS_cfg.h
@@ -0,0 +1,15 @@
+/**
+ * @file TIMERS_cfg.h
+ * @author Abdelrahman Elhetamy
+ * @version 1.00
+ */
+
+#ifndef TIMERS_CFG_H_
+#define TIMERS_CFG_H_
+
+#define ICU_SENSE_CTRL		ICU_RISING
+
+
+#define ICU_INT_INIT_STATE		ENABLED
+
+#endif /* TIMERS_CFG_H_ */
